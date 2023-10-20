b623e624d7e6afd37bc5926180efbfc3d0933771
NorthXB <115947351+NorthXB@users.noreply.github.com>
2023-10-21 01:47:35 +0700

    Add files via upload

---
+++ langsung/p2p/p2p.js
@@ -0,0 +1,42 @@
+function p2p0(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/0.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p1(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/1.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p2(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/2.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p3(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/3.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p4(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/4.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p5(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/5.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p6(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/6.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p7(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/7.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p8(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/8.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p9(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/9.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p10(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/10.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p11(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/11.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p12(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/12.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p13(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/13.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p14(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/14.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p15(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/15.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p16(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/16.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p17(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/17.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p18(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/18.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p19(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/19.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
+function p2p20(){document.getElementById("ar-169")
+.innerHTML='<iframe src="https://northxb.github.io/langsung/p2p/20.html" allow="encrypted-media" scrolling="no" frameborder="0" height="100%" width="100%" gesture="media" allowfullscreen="allowfullscreen"></iframe>';}
